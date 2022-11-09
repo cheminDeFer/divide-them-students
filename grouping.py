b882fa307741c18a017d8283285782da6ef6c70d
Hüseyin Er <erhuseyinn@gmail.com>
2022-11-09 16:04:25 +0300

    Function div_students_by_2 deprecated
    Todo: remove it from main and implement shuffle command's --N argument

--- grouping.py
+++ grouping.py
@@ -1,12 +1,24 @@
 import random
-def div_students_by_2(students):
+import warnings
+def div_students_by_n(students, n : int):
+    assert n > 1, "n should be greater than 1"
     # calculate how many groups
-    group_number, rem = divmod(len(students), 2)
-    gcs = [2 for i in range(group_number)]
-    if rem == 1:
-        gcs[-1] += 1
+    group_number, rem = divmod(len(students), n)
+    gcs = [n for i in range(group_number)]
+    for i in range(rem):
+        gcs[-i] += 1
+
     groups = _populate_groups(students, gcs)
     return tuple(groups)
+
+def div_students_by_2(students):
+    warnings.warn(
+        "div_students_by_2 is deprecated please "
+        "use div_students_by_n instead.",
+        DeprecationWarning,
+        stacklevel=2
+        )
+    return div_students_by_n(students, 2)
 
 def _populate_groups(students, gcs):
     groups = [list() for i in range(len(gcs))] 
@@ -16,3 +28,4 @@ def _populate_groups(students, gcs):
             students.remove(s_candidate)
             g.append(s_candidate)
     return groups
+
